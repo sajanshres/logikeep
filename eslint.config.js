18a653fcd209345acf28593ab282563de18ce7dc
Sajan Kumar Shrestha <np03cs4a210005@heraldcollege.edu.np>
2026-07-09 09:37:44 +0000

    ignore coverage folder

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'convex/_generated']),
+  globalIgnores(['dist', 'convex/_generated', 'coverage']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
